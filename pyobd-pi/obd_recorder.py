f1409cd742a62d18ca92c496179273d7d7a0f608
Aysan Mahmoudzadeh <54597289+aysan-mz@users.noreply.github.com>
2020-11-15 18:27:25 +0200

    Update obd_recorder.py

--- pyobd-pi/obd_recorder.py
+++ pyobd-pi/obd_recorder.py
@@ -87,132 +87,25 @@ class OBD_Recorder():
 	    log_Flags = str(Set_Flag_Score)
             self.log_file.write(log_string+"\n")
 	    self.log_file_flag.write(log_Flags+"\n")
-
-    
-    def Set_Flag(self, rpm, temp):
-	if (rpm < 4000 and temp < 60):
-	    Flag_Blue = 1
-	    Set_Flag_record = 'Flag_Blue'
-	    Flag_Score = 0.1
-	    return Flag_Score
-########RPM Normal and Coolant Normal
-	elif (rpm < 4000 and (temp >= 60 and temp < 120)):
-	    Flag_Green = 1
-	    Set_Flag_record = 'Flag_Green'
-	    Flag_Score = 0
-	    return Flag_Score
-########RPM Normal and Coolant high
-	elif (rpm < 4000 and temp >= 120):
-	    Flag_Yellow = 1
-	    Set_Flag_record = 'Flag_Yellow'
-	    Flag_Score = 0.3
-	    return Flag_Score
-########RPM high and Coolant Low
-	elif ((rpm >= 4000 and rpm < 6000) and temp < 60):
-	    Flag_Orange = 1
-	    Set_Flag_record = 'Flag_Orange'
-	    Flag_Score = 0.6
-	    return Flag_Score
-########RPM high and Coolant Normal
-	elif ((rpm >= 4000 and rpm < 6000) & (temp >= 60 and temp < 120)):
-	    Flag_Blue = 1
-	    Set_Flag_record = 'Flag_Blue'
-	    Flag_Score = 0.1
-	    return Flag_Score
-########RPM high and Coolant high
-	elif ((rpm >= 4000 and rpm < 6000) and temp >= 120):
-	    Flag_Orange = 1
-	    Set_Flag_record ='Flag_Orange'
-	    Flag_Score = 0.6
-	    return Flag_Score
-########RPM too high and Coolant Low
-	elif (rpm >= 6000 and temp < 60):
-	    Flag_Red = 1
-	    Set_Flag_record = 'Flag_Red'
-	    Flag_Score = 1
-	    return Flag_Score
-########RPM too high and Coolant Normal
-	elif (rpm >= 6000 and (temp >= 60 and temp < 120)):
-	    Flag_Orange = 1
-	    Set_Flag_record = 'Flag_Orange'
-	    Flag_Score = 0.6
-	    return Flag_Score
-########RPM too high and Coolant high
-	elif (rpm >= 6000 and temp >= 120):
-	    Flag_Red = 1
-	    Set_Flag_record = 'Flag_Red'
-	    Flag_Score = 1
-	    return Flag_Score
-
-
-    
-def Average_Score():
-    import glob
-    import os.path
-    import os
-    current_directory = os.path.abspath('/home/pi/Desktop/pyobd-pi/Flags')
-    current_directory1 = os.path.abspath('/home/pi/Desktop/pyobd-pi/Last-score')
-    current_directory2 = os.path.abspath('/home/pi/Desktop/pyobd-pi/Last-len')
-    #print(current_directory)
-    filename_list = glob.glob1(current_directory,'car-*.txt')
-    log_lenght = open(current_directory2 + '/' +'Len', "r")
-    last_lenght = log_lenght.readline()
-    log_lenght.close()
-    #print filename_list
-    mean = 0
-    current_len = 0
-    last_score_file = open(current_directory1 +'/'+'Score','r')
-    last_score = last_score_file.readline()
-    last_score_file.close()
-    for files in filename_list:
-	infile = open(current_directory+'/'+files, 'r')
-	#print infile
-
-	numbers = [float(line) for line in infile.readlines()]
-	infile.close()
-	current_len = len(numbers)
-	mean = sum(numbers)/len(numbers)
-	#print(mean)
-	#infile.deleted()
-	myfile = "current_directory/filename_list"
-	os.remove(myfile)
-
-    localtime = time.localtime(time.time())
-    #log_lenght.write(str(float(last_lenght)))
-    filename = "/home/pi/Desktop/pyobd-pi/Score/Score_Trip_" + str(localtime[0])+"-"+str(localtime[1])+"-"+str(localtime[2])+"-"+str(localtime[3])+"-"+str(localtime[4])+"-"+str(localtime[5])+".log"
-    final_score = ((mean + float(last_score)) / 2)     
-    Flag_Trip = open(filename, "w", 128)
-    localtime = datetime.now()
-    current_time = str(localtime.hour)+":"+str(localtime.minute)+":"+str(localtime.second)
-    log_string = "Time:" + str(current_time) + "\t" + "Score OBD: " + str(final_score)
-    Flag_Trip.write(log_string + "\n")
-   
+  
 
 		
 username = getpass.getuser()  
 logitems = ["rpm", "speed", "temp", "dtc_status", "engine_mil_time", "Throttle_posi"]
-logFlagScore = ["Set_Flag_Score"]
 
 o = OBD_Recorder('/home/pi/Desktop/pyobd-pi/log/Record_Data/', logitems)
-c = OBD_Recorder('/home/pi/Desktop/pyobd-pi/Flags/', logFlagScore)
 o.connect()
-c.connect()
 
 if(not o.is_connected()):
     localtime = datetime.now()
     current_time = str(localtime.year)+"-"+str(localtime.month)+"-"+str(localtime.day)+"-"+str(localtime.hour)+":"+str(localtime.minute)+":"+str(localtime.second)
     log_string = current_time
     logerror = "Time:" + str(current_time) + "\t"+ "The OBD is not connected"
-    logtimenotcon = '/home/pi/Desktop/pyobd-pi/Score/'+'Score_Trip_Error-'+current_time
-    logtime_file = open(logtimenotcon, "w", 128)
     logtime_file.write(logerror+"\n")
     logtime_file.close()
     print "Not connected"
     
-Average_Score()
 o.record_data()
-c.record_data()
-Average_Score()
 #if not c.is_connected():
 #    print "Not recorded Flags"
 #c.record_data()
